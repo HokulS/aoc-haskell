9c501c5c2bb8020efe0c9be56eb412bc5bd3283b
hokul <hokulis@outlook.com>
2025-12-13 10:11:27 +1000

    Day 1 P2
    Got a bit of help for this one

--- 2025/1/main.hs
+++ 2025/1/main.hs
@@ -1,18 +1,32 @@
-main :: IO()
+main :: IO ()
 main = do
-    short <- readFile "short.txt"
-    let short_lines = lines short
-    print $ partOne short_lines
+  short <- readFile "short.txt"
+  print $ partOne short
+  (dial, p2) <- return . foldl partTwo (50, 0) . map readRot . lines $ short
+  print p2
 
-    long <- readFile "long.txt"
-    let long_lines = lines long
-    print $ partOne long_lines
+  long <- readFile "long.txt"
+  print $ partOne long
+  (dial, p2) <- return . foldl partTwo (50, 0) . map readRot . lines $ long
+  print p2
 
-partOne :: [String] -> Int
-partOne x = length $ filter (== 0) $ map modulus $ scanl (+) 50 $ map parseDirection x
-
+-- Part One
 parseDirection :: String -> Int
-parseDirection (x:xs) = if x == 'L' then -1 * read xs else read xs
+parseDirection (x : xs) = if x == 'L' then -(read xs) else read xs
 
-modulus :: Int -> Int
-modulus x = mod x 100
+partOne :: String -> Int
+partOne = length . filter (== 0) . map (`mod` 100) . scanl (+) 50 . map parseDirection . lines
+
+-- Part Two
+type Rot = (Int -> Int, Int)
+
+type State = (Int, Int)
+
+readRot :: String -> Rot
+readRot (d : s) = (if d == 'R' then id else negate, read s)
+
+partTwo :: State -> Rot -> State
+partTwo (dial, acc) (f, n) = (dial', acc + change)
+  where
+    dial' = (dial + f n) `mod` 100
+    change = (f dial `mod` 100 + n) `div` 100
