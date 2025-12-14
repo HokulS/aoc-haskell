8a65ded2a17eb65f1f040cc1af1c2dfe8ec06aba
hokul <hokulis@outlook.com>
2025-12-14 15:21:07 +1000

    Day 2 P2

--- 2025/2/main.hs
+++ 2025/2/main.hs
@@ -1,11 +1,8 @@
+import Data.List
+
 type State = (Int, Int)
 
 type Range = (Int, Int)
-
-main = do
-  (p1, p2) <- foldl go (0, 0) . map splitHyphen . splitCommas <$> getContents
-  print p1
-  print p2
 
 splitCommas :: String -> [String]
 splitCommas s = case dropWhile (`elem` ",\n") s of
@@ -22,10 +19,26 @@ splitHyphen s = (read x, read ys)
 numberOfDigits :: Int -> Int
 numberOfDigits = length . show
 
+-- Part 2
+isSubString :: String -> Int -> Bool
+isSubString x increment = (mod (length x) increment == 0) && all (uncurry (==)) (zip (cycle sub) remain)
+  where
+    (sub, remain) = splitAt increment x
+
+checkAll :: String -> Bool
+checkAll x = any (isSubString x) [1 .. (length x `div` 2)]
+
 go :: State -> Range -> State
 go (p1, p2) (x, y) = (p1 + p1', p2 + p2')
   where
-    p1' = sum . filter (\z -> z `mod` digits z == z `div` digits z) $ [x .. y]
+    p1' = sum . filter checkInvalid1 $ [x .. y]
       where
-        digits z = 10 ^ (numberOfDigits z `div` 2)
-    p2' = 1
+        checkInvalid1 z = z `mod` digits == z `div` digits
+          where
+            digits = 10 ^ (numberOfDigits z `div` 2)
+    p2' = sum . filter (checkAll . show) $ [x .. y]
+
+main = do
+  (p1, p2) <- foldl' go (0, 0) . map splitHyphen . splitCommas <$> getContents
+  print p1
+  print p2
